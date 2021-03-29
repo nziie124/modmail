067764f134188132a9c23542781eb060a02f7f50
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-03-29 16:24:56 -0700

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -622,7 +622,7 @@ class Modmail(commands.Cog):
     @commands.command()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     async def raw(self, ctx, message_id: int=None):
-        """`[New]` - 📄 Prints out the raw content of an embed."""
+        """`[New]` - 📄 Prints out the raw content of an embed (codeblock)."""
         if message_id is None:
             return await ctx.send("Please provide a message ID")
         
@@ -632,6 +632,20 @@ class Modmail(commands.Cog):
             print(str(e))
         msgg = msg.embeds[0]
         await ctx.send(f"```{msgg.description}```")
+    
+@raw.command()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def raw_plain(self, ctx, message_id: int=None):
+        """`[New]` - 📄 Prints out the raw content of an embed (non-codeblock)."""
+        if message_id is None:
+            return await ctx.send("Please provide a message ID")
+        
+        try:
+            msg = await ctx.fetch_message(message_id)
+        except Exception as e:
+            print(str(e))
+        msgg = msg.embeds[0]
+        await ctx.send(f"{msgg.description}")
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.OWNER)
