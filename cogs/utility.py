3227926d41e4ef646dc7d1c0248864e6f720705c
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-03 16:50:57 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -665,6 +665,16 @@ class Utility(commands.Cog):
         return await ctx.send(embed=embed)
 
     @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def inactivity_notice(self, ctx, member:discord.Member):
+            """A command that lets a specified Staff member know that they are inactive."""
+    try:
+        await member.send('What’s Up!\n\nThis is an automated message from the `WA Management Team`.\n\n*It has recently come to our attention that you have been a little inactive!* 💤 \n\nWe hope you are doing 👌 and staying safe. We would appreciate if you would  There are going to be directions in the announcements channel `[you will see after you verify]` on how to fill out a break request correctly.\n\n*Cheers!*\n`~` **Operations Management**\n*Wumpus Ads*')
+        await ctx.send(f'**Notice sent to `{member.name}`!**')
+    except:
+        await ctx.send('**Their dms are closed or they blocked the bot.**')
+
+    @commands.command()
     @checks.has_permissions(PermissionLevel.OWNER)
     async def prefix(self, ctx, *, prefix=None):
         """
