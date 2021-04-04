03e761830dd47adcde6850888ebeda8cbb625526
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-04 15:47:36 -0700

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -667,12 +667,13 @@ class Modmail(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(Permission.Level.ADMINISTRATOR)
-    async def test(self, ctx, member: discord.Member):
+    async def testrole(self, ctx, member: discord.Member):
       """
       Adds the given roles to the user.
       """
       roles = [782059709272031303, 826454962342592582]
       await member.add_roles(*[member.guild.get_role(role) for role in roles])
+            await ctx.send("Done!")
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
